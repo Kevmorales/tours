d28478a07d2f66f6e4e5474ea5af183e1e627207
Kevin Morales <kevinmoralesh@gmail.com>
2021-09-17 11:09:53 +0200

    added not interested and refresh button when no tours

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+
+const removeTour = (id) => {
+
+  const newTours = tours.filter((tour) => tour.id !== id);
+  setTours(newTours)
+
+}
+
   const fetchTours = async () => {
     setLoading(true);
 
@@ -38,12 +46,26 @@ fetchTours();
     )
 
   }
+  if(tours.length === 0) {
+
+    return (
+
+      <main>
+        <div className="title">
+          <h2>No tours left.</h2>
+          <button className="btn" onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+
+    )
+
+  }
 
   return (
   
   <main>
     
-    <Tours tours={tours}/>
+    <Tours tours={tours} removeTour={removeTour}/>
     
     </main>
 
