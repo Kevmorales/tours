38ff7a77bdbc5513973cb1e83cd179c9371f0953
Kevin Morales <kevinmoralesh@gmail.com>
2021-09-17 10:36:45 +0200

    some fixes and updates to show the list

--- src/App.js
+++ src/App.js
@@ -6,31 +6,28 @@ import Tours from './Tours'
 const url = 'https://course-api.com/react-tours-project'
 function App() {
 
-  const [loading,setLoading] = useState(true);
-  const [tours,setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
 
-  const fetchTours = async() => {
-    setLoading(true)
+  const fetchTours = async () => {
+    setLoading(true);
 
-    try {
+
+      try {
       const response = await fetch(url);
-      const tours = await response.json();
+      const tours = await response.json()
       setLoading(false);
       setTours(tours);
-
     } catch (error) {
-
       setLoading(false);
       console.log(error);
-
     }
-   
-  }
-useEffect(()=> {
+  };
+useEffect(() => {
 
 fetchTours();
 
-},[]);
+}, []);
 
   if(loading){
 
@@ -42,7 +39,15 @@ fetchTours();
 
   }
 
-  return <main><Tours /></main>
+  return (
+  
+  <main>
+    
+    <Tours tours={tours}/>
+    
+    </main>
+
+  ); 
 }
 
 export default App
